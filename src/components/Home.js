735fb134d7d1b1284ee6bc3b5a1d702b16c98be2
Derek Nguyen <derek.nguyen02@gmail.com>
2023-06-13 19:40:25 -0700

    added Links to pages and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Fragment } from 'react';
+import { Link } from 'react-router-dom'
 
 const Home = ({}) => {
     return (
-        <>
+        <>  
             <div>
                 <div><h1>This is fitness tracker</h1></div>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import {
     Home,
@@ -9,16 +9,25 @@ import {
 
 
 const App = () => {
+
     return (
         <div className='app'>
+            <nav className='navbar'>
+                <Link to='/'>Home</Link>
+                <Link to='/routines'>Routines</Link>
+                <Link to='/my-routines'>My Routines</Link>
+                <Link to='/activities'>Activities</Link>
+                <Link to='/register'>Register</Link>
+                <Link to='/login'>Login</Link>
+            </nav>
             <Router>
                 <Switch>
-                    <Route path='/activities' render={(props) => (
-                        <ViewActivities />
+                    <Route exact path='/' render={(props) => (
+                        <Home />
                     )}>
                     </Route>
-                    <Route path='/' render={(props) => (
-                        <Home />
+                    <Route path='/activities' render={(props) => (
+                        <ViewActivities />
                     )}>
                     </Route>
                 </Switch>
